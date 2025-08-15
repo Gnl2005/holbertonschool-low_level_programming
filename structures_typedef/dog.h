a0ae05390326eb24676b6580f24b9ddaa6e3b0ae
Gnl2005 <allahverdiyevasofiya@gmail.com>
2025-08-15 15:11:09 +0000

    K

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,18 +1,17 @@
 #ifndef DOG_H
 #define DOG_H
 
-/**
- * struct dog - structure to store dog information
- * @name: name of the dog
- * @age: age of the dog
- * @owner: owner of the dog
- */
+#include <stddef.h>  /* NULL üçün */
+
 struct dog
 {
-char *name;
-float age;
-char *owner;
+	char *name;
+	float age;
+	char *owner;
 };
 
+void init_dog(struct dog *d, char *name, float age, char *owner);
+
 #endif /* DOG_H */
+
 
