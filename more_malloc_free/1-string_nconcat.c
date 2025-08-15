3838b494add70dbe2c7145bc78384ae791e17443
Gnl2005 <allahverdiyevasofiya@gmail.com>
2025-08-15 14:41:31 +0000

    K

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -1,39 +1,52 @@
 #include <stdlib.h>
-#include <string.h>
 
 /**
- * string_nconcat - concatenates s1 and the first n bytes of s2
+ * _strlen - calculates length of a string
+ * @s: input string
+ *
+ * Return: length of the string
+ */
+unsigned int _strlen(char *s)
+{
+	unsigned int len = 0;
+
+	if (s == NULL)
+		return (0);
+	while (s[len] != '\0')
+		len++;
+	return (len);
+}
+
+/**
+ * string_nconcat - concatenates s1 and first n bytes of s2
  * @s1: first string (can be NULL)
  * @s2: second string (can be NULL)
  * @n: number of bytes to copy from s2
  *
- * Return: pointer to newly allocated string or NULL on failure
+ * Return: pointer to newly allocated string, or NULL if failure
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *new_str;
-	unsigned int len1, len2, total_len;
+	unsigned int len1, len2, i, j;
 
-	if (s1 == NULL)
-		s1 = "";
-	if (s2 == NULL)
-		s2 = "";
+	len1 = _strlen(s1);
+	len2 = _strlen(s2);
 
-	len1 = strlen(s1);
-	len2 = strlen(s2);
-
-	if (n >= len2)
+	if (n > len2)
 		n = len2;
 
-	total_len = len1 + n + 1; /* +1 for null terminator */
-
-	new_str = malloc(total_len);
+	new_str = malloc(len1 + n + 1);
 	if (new_str == NULL)
 		return (NULL);
 
-	memcpy(new_str, s1, len1);
-	memcpy(new_str + len1, s2, n);
-	new_str[len1 + n] = '\0';
+	for (i = 0; i < len1; i++)
+		new_str[i] = s1[i];
+
+	for (j = 0; j < n; j++)
+		new_str[i + j] = s2[j];
+
+	new_str[i + j] = '\0';
 
 	return (new_str);
 }
