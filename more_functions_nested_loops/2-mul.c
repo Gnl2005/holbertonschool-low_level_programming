5feebd0d226c992d66b4c9bf7c1c3cefe494e9f8
Gnl2005 <allahverdiyevasofiya@gmail.com>
2025-08-18 14:58:25 +0000

    a+b

--- more_functions_nested_loops/2-mul.c
+++ more_functions_nested_loops/2-mul.c
@@ -1,3 +1,14 @@
 #include "main.h"
 
+/**
+ * mul - iki tam ədədin hasilini hesablayır
+ * @a: birinci ədəd
+ * @b: ikinci ədəd
+ *
+ * Return: a və b-nin hasilini qaytarır
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
 
