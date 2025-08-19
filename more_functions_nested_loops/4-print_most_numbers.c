a9863022aedf108e9ba91fdf5d8b022e60b8509e
Gnl2005 <allahverdiyevasofiya@gmail.com>
2025-08-19 14:52:54 +0000

    Hello

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_most_numbers - prints the numbers, from 0 to 9, followed by a new line
+ */
+void print_most_numbers(void)
+{
+	_putchar('0' '1' '3' '5' '6' '7' '8' '9');
+	_putchar('\n');
+}
