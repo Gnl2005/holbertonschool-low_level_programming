8b579d149faa6f5182fe4627f3b8bb2fe7a2427c
Gnl2005 <allahverdiyevasofiya@gmail.com>
2025-08-22 14:24:40 +0000

    action=array[i]

---
+++ function_pointers/1-array_iterator.c
@@ -0,0 +1,21 @@
+#include "function_pointers.h"
+#include <stddef.h>
+#include <stdlib.h>
+
+/**
+ * array_iterator - a function given as a parameter on each element of an array
+ * @array: each element of an array
+ * @size: the size of the array
+ * @action: a pointer to the function to apply
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	if (array == NULL)
+		return;
+	else if (action == NULL)
+		return;
+	for (size_t i = 0; i < size; i++)
+	{
+		action(array[i]);
+	}
+}
