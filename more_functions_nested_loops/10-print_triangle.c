218a2457a560a239efb433e623eb25e381829ea0
Gnl2005 <allahverdiyevasofiya@gmail.com>
2025-08-26 16:43:19 +0000

    Ucbucaq

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * print_triangle - a function that prints a triangle
+ * @size: the size of the triangle
+ */
+void print_triangle(int size)
+{
+	int a, b;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
+	for (a = 0; a < size; a++)
+	{
+		for (b = 0; b < a; b++)
+			_putchar(' ');
+		_putchar('#');
+		_putchar('\n);
+	}
+}
