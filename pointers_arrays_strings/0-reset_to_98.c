13a8572b6036d1b6988223745d78a9e765bf911a
Gnl2005 <allahverdiyevasofiya@gmail.com>
2025-08-29 19:01:29 +0000

    *n=98

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * reset_to_98 - updates the value it points to to 98
+ * @n: as parameter
+ */
+void reset_to_98(int *n)
+{
+	int *n = 98;
