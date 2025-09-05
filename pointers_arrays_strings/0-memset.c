f045a27c5340f843940b222fa0a5b73f60bf9c60
Gnl2005 <allahverdiyevasofiya@gmail.com>
2025-09-05 22:04:22 +0000

    k

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -1,20 +1,19 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "main.h"
 
 /**
- * _memset - a function that fills memory with a constant byte
- * @s: a pointer
- * @b: the constant byte
- * @n: function fills the first
+ * _memset - bruh
+ * @s: s
+ * @b: byte
+ * @n: numberr
  *
- * Return: a pointer to the memory area s
+ * Return: return answer
  */
+
 char *_memset(char *s, char b, unsigned int n)
 {
-	for (int i = 0; i < n; i++)
-	{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
 		s[i] = b;
-	}
 	return (s);
 }
