cc3bddcc9a830f174d50deafe0b40f8b2ec0859b
Gnl2005 <allahverdiyevasofiya@gmail.com>
2025-09-05 21:36:09 +0000

    k

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -11,14 +11,13 @@
  */
 char *_strncat(char *dest, char *src, int n)
 {
-	int a = 0;
-	int b = 0;
-	
+	int a = 0, b = 0;
+
 	while (dest[a] != '\0')
 	{
 		a++;
 	}
-	
+
 	while (b < n && src[b] != '\0')
 	{
 		dest[a] = src[b];
