a05a8057e8f8f937ef6dcba8a98539eb3f6c5a4f
Gnl2005 <allahverdiyevasofiya@gmail.com>
2025-09-05 21:52:34 +0000

    k

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -10,12 +10,12 @@ char *cap_string(char *str)
 {
 	int i = 0, j;
 	char sep[] = " \t\n,;.!?\"(){}";
-	
+
 	while (str[i] != '\0')
 	{
 		if (i == 0 && str[i] >= 'a' && str[i] <= 'z')
 			str[i] -= 32;
-		
+
 		for (j = 0; sep[j] != '\0'; j++)
 		{
 			if (str[i] == sep[j] && str[i + 1] >= 'a' && str[i + 1] <= 'z')
