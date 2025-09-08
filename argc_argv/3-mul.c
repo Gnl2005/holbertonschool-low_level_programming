641e538e0301cecd5722585fe8c0e3e3bfdeba3a
Gnl2005 <allahverdiyevasofiya@gmail.com>
2025-09-08 23:15:10 +0000

    a * b

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Enter point
+ * @argc: Argument count
+ * @argv: Argument massiv
+ * 
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+	int a, b, n;
+
+	if (argc != 3)
+	{
+		printf("Eror\n");
+		return (1);
+	}
+	a = atoi(argv[1]);
+	b = atoi(argv[2]);
+	n = a * b;
+
+	printf("%d\n", n);
+	return (0);
+}
