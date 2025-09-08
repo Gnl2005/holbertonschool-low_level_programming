0b4220e40d6ced2395047314743410dcc39a8e4c
Gnl2005 <allahverdiyevasofiya@gmail.com>
2025-09-08 22:46:10 +0000

    i-1

---
+++ argc_argv/1-args.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Enter point
+ * @argc: Argument count
+ * @argv: Argument massiv
+ *
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+	(voi)argv;
+
+	for (i = 0; i <= argc; i++)
+	{
+		printf("%s\n", i - 1);
+	}
+	return (0);
+}
