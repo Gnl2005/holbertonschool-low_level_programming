5ca2b273608dc1ac151ee17f405ed1c4fcfbbc2d
Gnl2005 <allahverdiyevasofiya@gmail.com>
2025-09-08 23:29:20 +0000

    a + b

---
+++ argc_argv/4-add.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Enter point
+ * @argc: Argument count
+ * @argv: Argument massiv
+ *
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+	int a, b, n;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	a = atoi(argv[1]);
+	b = atoi(argv[2]);
+	n = a + b;
+
+	printf("%d\n", n);
+	return (0);
+}
